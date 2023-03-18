2af63a24fb1071e85df5fc42788a84b1868ed42e
ismail <my>
2023-03-17 20:04:56 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,16 +1,13 @@
 int main() {
-    for(int i = 0; i < 100; i++) {
-        for(int j = i; j < 100; j++) {
-            int n1 = i / 10;
-            int n2 = i % 10;
-            int n3 = j / 10;
-            int n4 = j % 10;
-            putchar(n1 + '0');
-            putchar(n2 + '0');
+    int i, j;
+    for (i = 0; i < 100; i++) {
+        for (j = i + 1; j < 100; j++) {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
             putchar(' ');
-            putchar(n3 + '0');
-            putchar(n4 + '0');
-            if(i < 99) {
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (j < 99) {
                 putchar(',');
                 putchar(' ');
             }
