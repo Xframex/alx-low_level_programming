c47cc6e3ae18e998d5117955ce3f6e69c69f0656
ismail <my>
2023-03-17 20:17:27 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 
-int main() {
-    int i, j, k, l;
-    for (i = 0; i < 100; i++) {
-        for (j = 0; j < 100; j++) {
-            if (i <= j) {
-                continue;
-            }
-            for (k = 0; k < 100; k++) {
-                if (k <= j) {
-                    continue;
-                }
-                for (l = 0; l < 100; l++) {
-                    if (l <= k) {
-                        continue;
-                    }
-                    putchar(i / 10 + '0');
-                    putchar(i % 10 + '0');
-                    putchar(' ');
-                    putchar(j / 10 + '0');
-                    putchar(j % 10 + '0');
-                    putchar(' ');
-                    putchar(k / 10 + '0');
-                    putchar(k % 10 + '0');
-                    putchar(' ');
-                    putchar(l / 10 + '0');
-                    putchar(l % 10 + '0');
-                    if (i < 99 || j < 98 || k < 97 || l < 96) {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
-    return 0;
-}
+/**
+ * main - a simple program that outputs unordered
+ * combinations of two digit integers without duplicates
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int ifirst;
+	int i;
+	int jfirst;
+	int j;
 
+	for (ifirst = 48; ifirst < 58; ifirst++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			j = i + 1;
+			jfirst = ifirst;
+			for (; jfirst < 58; jfirst++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(ifirst);
+					putchar(i);
+					putchar(' ');
+					putchar(jfirst);
+					putchar(j);
+					if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
