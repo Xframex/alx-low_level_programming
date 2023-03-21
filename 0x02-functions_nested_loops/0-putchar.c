d125406e49e60d3b0de73f96560122b1ea4c4295
ismail <my>
2023-03-21 14:51:48 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+    char str[20] = "_putchar";
+
+    // puts is used to print the string char array
+    puts(str);
+
+    return 0;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
- Hello C
+low dev 
