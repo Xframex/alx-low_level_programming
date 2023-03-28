3cf1c82b7c019e8531b3eecdc19fb9cc8ef85216
ismail <my>
2023-03-27 21:51:13 -0700

    array

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,39 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
+#include <stdio.h>
 
 /**
- * main - print password.
- *
- * Return: 0.
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int ascii = 2772, i = 0, j, random;
-	char password[100];
-	time_t t;
+	int n;
 
-	srand((int) time(&t));
-	while (ascii > 126)
-	{
-		random = rand() % 126;
-		password[i] = random;
-		ascii -= random;
-		i++;
-	}
-	if (ascii > 0)
-		password[i] = ascii;
-	else
-	{
-		i--;
-	}
-	
-
-	for (j = 0; j <= i; j++)
-	{
-		printf("%c", password[j]);
-	}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	return (0);
 }
