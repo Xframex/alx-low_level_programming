b3633b017b40b5ef5e24e083e14cfa028f88b4f1
Xframex <loukili.issmael@gmail.com>
2023-09-08 13:19:41 -0700

    create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Function that cretes an empty hash table 
+ * @size: Size of the array to be created inside table
+ * Return: Pointe to the hash table (
+Success), NUll if fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (!size)
+		return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
+
