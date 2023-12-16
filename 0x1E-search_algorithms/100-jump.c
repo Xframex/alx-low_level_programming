0fbba82a885dc71cc90c94fb3e036e6ca8d08442
Xframex <loukili.issmael@gmail.com>
2023-12-16 10:37:26 -0800

    jump to search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value in an array of
+ * integers using the Justepp search algorithstep
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the nustepber
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int index, step, low, prev;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = (int)sqrt((double)size);
+	low = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value check lowed array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		low++;
+		prev = index;
+		index = low * step;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
+
+	return (-1);
+}
